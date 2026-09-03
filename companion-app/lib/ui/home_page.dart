8c048c2f788062796a19d98185aa5d554b29c434
rgdi <rgdi@users.noreply.github.com>
2026-09-03 13:18:48 +0000

    fix(companion): remove unused shared_preferences import

--- companion-app/lib/ui/home_page.dart
+++ companion-app/lib/ui/home_page.dart
@@ -1,7 +1,6 @@
 // HomePage: detecta vaults, muestra estado, permite instalar/actualizar.
 
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../models/plugin_release.dart';
 import '../services/vault_detector.dart';
 import '../services/updater.dart';
