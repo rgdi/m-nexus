1e293a7e1f7a0db0114637dee306f9776d6ce26f
rgdi <rgdi@users.noreply.github.com>
2026-09-05 14:54:54 +0000

    fix(v0.41): import VaultInfo from vault_detector.dart

--- companion-app/lib/screens/vault_list_page.dart
+++ companion-app/lib/screens/vault_list_page.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' as p;
-import '../models/vault_info.dart';
+import '../services/vault_detector.dart' show VaultInfo;
 import '../services/vault_detector.dart';
 import '../ui/vault_browser_page.dart';
 
