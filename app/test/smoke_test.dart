da313254736103aba26740d10273d6f2d5dc0076
rgdi <rgdi@users.noreply.github.com>
2026-09-06 19:49:12 +0000

    test: bare minimum test

--- app/test/smoke_test.dart
+++ app/test/smoke_test.dart
@@ -1,9 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mnexus_app/services/flashcard_service.dart';
-
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  test('FlashcardService class exists', () {
-    expect(FlashcardService, isNotNull);
-  });
+  test('1+1=2', () { expect(1+1, 2); });
 }
