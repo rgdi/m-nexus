0cb2b326073ea20c3450edd19e0ca73d563a843b
rgdi <rgdi@users.noreply.github.com>
2026-09-06 17:45:57 +0000

    fix(test v0.44.2): make setUp async for shared_preferences mock

--- app/test/settings_service_test.dart
+++ app/test/settings_service_test.dart
@@ -1,6 +1,6 @@
 // SettingsService: tests de persistencia.
 
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mnexus_app/services/settings_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,7 +8,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
+  setUp(() async {
     SharedPreferences.setMockInitialValues({});
   });
 
